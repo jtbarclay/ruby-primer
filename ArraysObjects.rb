59f00bdfd50c44368df3b392b1586f749355b8c5
Kai Peterson <pete9372@umn.edu>
2019-12-04 13:40:52 -0600

    complete array/hash file

---
+++ ArraysObjects.rb
@@ -0,0 +1,51 @@
+array1 = [1, 2, 3, 4]
+array2 = [3, 4]
+
+# find index of array
+print array1[0]
+puts ''
+
+# append to end of array
+arrayAppend = array2 << 5
+print arrayAppend
+puts '' 
+
+# array subtraction
+arraySubtraction = array1 - array2
+print arraySubtraction
+puts ''
+
+# array multiplication
+arrayMultiplication = array1 * 3
+print arrayMultiplication
+puts ''
+
+# array intersection
+arrayIntersection = array1 & array2
+print arrayIntersection
+puts ''
+
+# array rotation
+arrayRotation = array1.rotate(1)
+print arrayRotation
+puts ''
+
+# hashes (similar to objects in JavaScript)
+hash1 = { "one" => 1, "two" => 2, "three" => 3}
+hash2 = { "four" => 4, "five" => 5}
+print hash1
+puts ''
+
+# find key value in hash
+print hash1["one"]
+puts ''
+
+# merge two hashes
+hashMerged = hash1.merge(hash2)
+print hashMerged
+puts ''
+
+# keys and values can be any data type
+hash3 = { "one" => 1, 2 => true, :three => [1, 2, 3], false => "four"}
+print hash3
+puts ''
