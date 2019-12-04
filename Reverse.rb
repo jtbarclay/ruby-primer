c94d6e4be4f8d73208a6364908ecc836d33b9dda
Jon Barclay <my@nameisjon.com>
2019-12-04 13:28:56 -0600

    reverse

---
+++ Reverse.rb
@@ -0,0 +1,8 @@
+array1 = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+
+def arrayReverser(arr)
+    return arr.reverse()
+end
+
+puts array1
+puts arrayReverser(array1)
